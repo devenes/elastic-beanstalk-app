0f67011fd3c8fa634d36f4e59c4e9f419a98c2fb
Enes T <66560757+devenes@users.noreply.github.com>
2022-03-18 02:45:26 +0300

    Update eb_app.tf
    Update comments

--- eb_app.tf
+++ eb_app.tf
@@ -1,62 +1,60 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      source  = "hashicorp/aws" # required
+      version = "~> 3.27"       # required
     }
   }
 
   backend "s3" {
-    bucket = "enes-mybucket"
-    key    = "path/to/my/key"
-    region = "us-east-2"
+    bucket = "enes-mybucket"  # S3 bucket name
+    key    = "path/to/my/key" # S3 key name
+    region = "us-east-2"      # S3 region
   }
 }
 
 # AWS Provider configuration
 provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+  profile = "default"   # AWS profile 
+  region  = "us-east-2" # AWS region
 }
 
 # Create S3 bucket for Python Flask app
 resource "aws_s3_bucket" "eb_bucket" {
-  bucket = "enes-eb-python-flask"
+  bucket = "enes-eb-python-flask" # Name of S3 bucket to create for Flask app deployment needs to be unique 
 }
 
 # Define App files to be uploaded to S3
 resource "aws_s3_bucket_object" "eb_bucket_obj" {
   bucket = aws_s3_bucket.eb_bucket.id
-  key    = "beanstalk/app.zip"
-  source = "app.zip"
+  key    = "beanstalk/app.zip" # S3 Bucket path to upload app files
+  source = "app.zip"           # Name of the file on GitHub repo to upload to S3
 }
 
 # Define Elastic Beanstalk application
 resource "aws_elastic_beanstalk_application" "eb_app" {
-  name        = "enes-eb-tf-app"
-  description = "simple flask app"
+  name        = "enes-eb-tf-app"   # Name of the Elastic Beanstalk application
+  description = "simple flask app" # Description of the Elastic Beanstalk application
 }
 
 # Create Elastic Beanstalk environment for application with defining environment settings
 resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
-  bucket      = aws_s3_bucket.eb_bucket.id
-  key         = aws_s3_bucket_object.eb_bucket_obj.id
-  application = aws_elastic_beanstalk_application.eb_app.name
-  name        = "enes-eb-tf-app-version-lable"
+  bucket      = aws_s3_bucket.eb_bucket.id                    # S3 bucket name
+  key         = aws_s3_bucket_object.eb_bucket_obj.id         # S3 key path 
+  application = aws_elastic_beanstalk_application.eb_app.name # Elastic Beanstalk application name
+  name        = "enes-eb-tf-app-version-lable"                # Version label for Elastic Beanstalk application
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenv" {
-  name        = "enes-eb-tf-env"
-  application = aws_elastic_beanstalk_application.eb_app.name
-  # Define current version of the platform
-  solution_stack_name = "64bit Amazon Linux 2 v3.3.11 running Python 3.8"
-  description         = "environment for flask app"
-  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name
+  name                = "enes-eb-tf-env"
+  application         = aws_elastic_beanstalk_application.eb_app.name             # Elastic Beanstalk application name
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.11 running Python 3.8"         # Define current version of the platform
+  description         = "environment for flask app"                               # Define environment description
+  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name # Define version label
 
   setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = "aws-elasticbeanstalk-ec2-role"
+    namespace = "aws:autoscaling:launchconfiguration" # Define namespace
+    name      = "IamInstanceProfile"                  # Define name
+    value     = "aws-elasticbeanstalk-ec2-role"       # Define value
   }
-
 }
